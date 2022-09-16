1a071ad6e38a43417b213dd7372585d8fced3ca1
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-16 13:34:55 -0300

    Feat: charts implanted in dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,41 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
+
+
+const Chart = dynamic(()=> import('react-apexcharts'))
+
+const options = {}
+
+const series = [
+    {name: 'series1', data: [31, 120, 10, 28, 61, 18, 109]}
+]
 
 export default function Dashboard(){
     return(
         <Flex direction='column' h='100vh'>
             <Header/>
             <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
-            <Sidebar/>
+                <Sidebar/>
+                <SimpleGrid flex='1' gap='4' minChildWidth='320px' alignItems='flex-start'>
+                    <Box
+                    p='8'
+                    bg='gray.800'
+                    borderRadius={8}
+                    >
+                        <Text fontSize='lg' mb='4'>Inscritos da semana</Text>
+                        <Chart options={options} series={series} type="area" height={160}/>
+                    </Box>
+                    <Box
+                    p='8'
+                    bg='gray.800'
+                    borderRadius={8}
+                    >
+                        <Text fontSize='lg' mb='4'>Taxa de abertura</Text>
+                    </Box>
+
+                </SimpleGrid>
             </Flex>
         </Flex>
     )
