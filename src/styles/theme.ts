91a12eb54200306e5434044a6f96feaecc3947b9
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-16 09:15:02 -0300

    Chore: Version defined

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+
+export const theme = extendTheme({
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.900',
+                color: 'gray.50'
+            }
+        }
+    }
+})
