9e292f794cbda2128109e9a95a603bdb63365c79
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-19 10:51:52 -0300

    Refactor: index header componentized

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { Flex } from "@chakra-ui/react";
+import { Logo } from "../Header/Logo";
+import { NotificationNav } from "../Header/NotificationNav";
+import { Profile } from "../Header/Profile";
+import { SearchBox } from "../Header/SearchBox";
+
+export function Header(){
+    return(
+        <Flex 
+            as='header' 
+            w='100%'
+            maxW={1480}
+            h='20'
+            mx='auto'
+            mt='4'
+            px='6'
+            align='center'
+        >
+            <Logo/>
+            <SearchBox/>
+            <Flex
+            align='center'
+            ml='auto'
+            >
+                <NotificationNav/>
+                <Profile />
+            </Flex>
+
+        </Flex>
+    );
+}
