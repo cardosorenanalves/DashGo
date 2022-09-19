196bcfc4184e4953048b90db41f80ceaa513ae7f
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-19 09:44:15 -0300

    Feat: Table of users create

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
-import { RiAddLine } from "react-icons/ri";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
@@ -19,11 +19,49 @@ export default function UserList(){
                         size='sm' 
                         fontSize='sm'
                         colorScheme='pink'
-                        leftIcon={<Icon as={RiAddLine}/>}
+                        leftIcon={<Icon as={RiAddLine} fontSize='20'/>}
                         >
                             Criar novo
                         </Button>
                     </Flex>
+
+                    <Table colorScheme='whiteAlpha' >
+                        <Thead>
+                            <Tr>
+                                <Th px='6' color='gray.300' width='8'>
+                                    <Checkbox colorScheme='pink' />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th w='8'></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px='6'>
+                                <Checkbox colorScheme='pink' />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight='bold'>Renan Cardoso</Text>
+                                        <Text fontSize='sm' color='gray.300'>renancardoso0307@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>19 de Setembro, 2022</Td>
+                                <Td> 
+                                    <Button 
+                                    as='a' 
+                                    size='sm' 
+                                    fontSize='sm'
+                                    colorScheme='purple'
+                                    leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                                    >
+                                       Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
                 </Box>
             </Flex>
         </Box>
