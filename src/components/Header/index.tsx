a0bed018b2a8f19f8b1a19b3b10fb0d25b6f54ea
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-19 11:14:13 -0300

    Feat: index header responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "../Header/Logo";
 import { NotificationNav } from "../Header/NotificationNav";
 import { Profile } from "../Header/Profile";
 import { SearchBox } from "../Header/SearchBox";
 
 export function Header(){
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return(
         <Flex 
             as='header' 
@@ -17,13 +22,13 @@ export function Header(){
             align='center'
         >
             <Logo/>
-            <SearchBox/>
+            {isWideVersion && <SearchBox/>}
             <Flex
             align='center'
             ml='auto'
             >
                 <NotificationNav/>
-                <Profile />
+                <Profile showProfileData={isWideVersion}/>
             </Flex>
 
         </Flex>
