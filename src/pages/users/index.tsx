80f56d711c124b73ddf7136b8af984f8b6d04bc5
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-20 09:26:26 -0300

    Feat: users responsive and navigation aplied

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList(){
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return(
         <Box>
             <Header/>
@@ -14,7 +20,7 @@ export default function UserList(){
                 <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
                     <Flex mb='8' justify='space-between' align='center'>
                         <Heading size='lg' fontWeight='normal'>Usuários</Heading>
-
+                    <Link href={'/users/create'}>
                         <Button 
                         as='a' 
                         size='sm' 
@@ -24,22 +30,23 @@ export default function UserList(){
                         >
                             Criar novo
                         </Button>
+                    </Link>       
                     </Flex>
 
                     <Table colorScheme='whiteAlpha' >
                         <Thead>
                             <Tr>
-                                <Th px='6' color='gray.300' width='8'>
+                                <Th px={['4','4','6']} color='gray.300' width='8'>
                                     <Checkbox colorScheme='pink' />
                                 </Th>
                                 <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
+                              {isWideVersion && <Th>Data de cadastro</Th>}
                                 <Th w='8'></Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td px='6'>
+                                <Td px={['4','4','6']}>
                                 <Checkbox colorScheme='pink' />
                                 </Td>
                                 <Td>
@@ -48,9 +55,11 @@ export default function UserList(){
                                         <Text fontSize='sm' color='gray.300'>renancardoso0307@gmail.com</Text>
                                     </Box>
                                 </Td>
-                                <Td>19 de Setembro, 2022</Td>
+                              {isWideVersion && <Td>19 de Setembro, 2022</Td>}
                                 <Td> 
-                                    <Button 
+                                  { isWideVersion && 
+                                  
+                                  <Button 
                                     as='a' 
                                     size='sm' 
                                     fontSize='sm'
@@ -59,6 +68,8 @@ export default function UserList(){
                                     >
                                        Editar
                                     </Button>
+                                    
+                                    }
                                 </Td>
                             </Tr>
                         </Tbody>
