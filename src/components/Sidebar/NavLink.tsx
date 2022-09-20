3242b2aa6096859b267adc4474b2a86b59ca046d
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-20 09:28:40 -0300

    Feat: create navigation for sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,20 @@
-import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { ElementType } from "react"
 
 interface NavLinkProps extends LinkProps{
     icon: ElementType;
     children: string;
+    href: string;
 }
 
-export function NavLink({icon, children, ...rest}: NavLinkProps){
+export function NavLink({icon,href, children, ...rest}: NavLinkProps){
     return(
-        <Link display='flex' alignItems='center' {...rest} >
-            <Icon as={icon} fontSize='20'/>
-            <Text ml='4' fontWeight='medium'>{children}</Text>
-        </Link>
+        <Link href={href} passHref>
+            <ChakraLink display='flex' alignItems='center' {...rest} >
+                <Icon as={icon} fontSize='20'/>
+                <Text ml='4' fontWeight='medium'>{children}</Text>
+            </ChakraLink>
+        </Link>      
     )
 }
