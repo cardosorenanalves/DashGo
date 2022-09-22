e6db55997b60482f53c7092e172eb29915a7ac67
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-22 14:51:02 -0300

    Refactor: create new hook useUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,31 +1,13 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { useQuery } from 'react-query'
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList(){ 
-    const {data, isLoading, isFetching, error} = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR',{
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                })
-            }
-        });
-        return users
-    })
+    const {data, isLoading, isFetching, error} = useUsers()
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -112,7 +94,11 @@ export default function UserList(){
                         })}
                     </Tbody>
                 </Table>
-                <Pagination />
+                <Pagination 
+                totalCountOfRegister={200}
+                currentPage={5}
+                onPageChange={()=>{}}
+                />
                 </>
                    )}
                 </Box>
