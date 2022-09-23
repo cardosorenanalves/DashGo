9cabd1112cb7f19f1e61197c87a5da0d18b5d47a
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-23 11:22:13 -0300

    Feat: new options property in useUsers

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,5 +1,5 @@
 import { StringifyOptions } from "querystring";
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
 import { api } from "../api";
 
 type User = {
@@ -41,8 +41,9 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
     };
 }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options?: UseQueryOptions) {
     return useQuery(['users', page], () => getUsers(page),{
         staleTime: 1000 * 60 *10, 
-    })
+        ...options
+    }) as UseQueryResult<GetUsersResponse, unknown>
 }
