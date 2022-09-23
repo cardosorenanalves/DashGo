e815d0f935b525ca5cf525469374413db6e84bfd
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-23 10:15:05 -0300

    Feat: pagination dinamyc

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -7,7 +8,9 @@ import { Sidebar } from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList(){ 
-    const {data, isLoading, isFetching, error} = useUsers()
+    const [page, setPage] = useState(1)
+    const {data, isLoading, isFetching, error} = useUsers(page)
+   
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -61,7 +64,7 @@ export default function UserList(){
                         </Tr>
                     </Thead>
                     <Tbody>
-                        {data.map(user => {
+                        {data.users.map(user => {
                           return(
                             <Tr key={user.id}>
                             <Td px={['4','4','6']}>
@@ -95,9 +98,9 @@ export default function UserList(){
                     </Tbody>
                 </Table>
                 <Pagination 
-                totalCountOfRegister={200}
-                currentPage={5}
-                onPageChange={()=>{}}
+                totalCountOfRegister={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
                 />
                 </>
                    )}
